992393999ba5b48e8fde8ca116fb5352ca84a594
Samuel Stidham <dqfan2012@gmail.com>
2024-06-15 00:18:49 -0400

    Update install_linux.sh
    Updated the redhat-related distros script.

--- install_linux.sh
+++ install_linux.sh
@@ -2,9 +2,58 @@
 
 # Function to install packages for Red Hat and Fedora-based distributions
 install_redhat_fedora() {
+    local distro="$1"
+
     sudo dnf group install -y "Development Tools" "Development Libraries"
     sudo dnf install -y clang clang-tools-extra llvm lldb lldb-devel cmake rust rust-gdb rust-lldb golang
-    sudo dnf install -y cppcheck valgrind flawfinder
+    sudo dnf install -y cppcheck valgrind flawfinder java-17-openjdk
+
+    # Distribution-specific installations
+    case "$distro" in
+        rhel|centos|redhat|rocky|almalinux)
+            # PostgreSQL installation
+            sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+            sudo dnf -qy module disable postgresql
+            sudo dnf install -y postgresql16-server
+            sudo /usr/pgsql-16/bin/postgresql-16-setup initdb
+            sudo systemctl enable postgresql-16
+            sudo systemctl start postgresql-16
+
+            # CodeReady Builder and EPEL repositories
+            case "$distro" in
+                rhel|redhat)
+                    sudo subscription-manager repos --enable codeready-builder-for-rhel-9-$(arch)-rpms
+                    sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+                    ;;
+                centos)
+                    sudo dnf config-manager --set-enabled crb
+                    sudo dnf install -y epel-release epel-next-release
+                    ;;
+                rocky|almalinux)
+                    sudo dnf config-manager --set-enabled crb
+                    sudo dnf install -y epel-release
+                    ;;
+            esac
+            ;;
+        fedora)
+            # PostgreSQL installation for Fedora
+            sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/F-40-x86_64/pgdg-fedora-repo-latest.noarch.rpm
+            sudo dnf install -y postgresql16-server
+            sudo /usr/pgsql-16/bin/postgresql-16-setup initdb
+            sudo systemctl enable postgresql-16
+            sudo systemctl start postgresql-16
+            ;;
+        *)
+            echo "Unsupported distribution: $distro"
+            exit 1
+            ;;
+    esac
+
+    # Installing sonar-scanner for Fedora has to be done manually.
+    # Install sonar-scanner for Red Hat-based distributions (excluding Fedora)
+    if [[ "$distro" != "fedora" ]]; then
+        sudo dnf install -y sonar-scanner
+    fi
 }
 
 # Function to install packages for Debian-based distributions
@@ -53,7 +102,7 @@ if [ -f /etc/os-release ]; then
     case "$ID" in
         # This will not work with Red Hat/CentOS earlier than version 8 or earlier than Fedora 22
         rhel|centos|redhat|fedora|rocky|almalinux)
-            install_redhat_fedora
+            install_redhat_fedora "$ID"
             ;;
         debian|ubuntu|linuxmint|pop|kali|mx|zorin)
             install_debian
